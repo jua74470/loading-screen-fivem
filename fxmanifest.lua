5cbda7db3a6510b90f010feca8989fdc3529e248
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 19:13:19 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven'
+description ''
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html'
+}
